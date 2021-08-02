a1753812486d951a751894cd8e9113b8e713781e
Kelvin Stephano <kstephano@gmail.com>
2021-08-02 10:53:17 +0100

    removed redundant fragment.

--- app/src/main/java/com/example/contactaddressbook/MainActivity.java
+++ app/src/main/java/com/example/contactaddressbook/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_phone, R.id.navigation_contacts, R.id.navigation_new_contact,
+                R.id.navigation_contacts, R.id.navigation_new_contact,
                 R.id.navigation_edit_contact)
                 .build();
         Toolbar toolbar = findViewById(R.id.toolbar_contacts);
